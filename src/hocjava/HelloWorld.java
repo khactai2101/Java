e4556767fd91b967759ae2f21ba4b2c026068079
Nguyen Khac Tai <nguyenkhactai1@gmail.com>
2024-10-23 14:48:42 +0700

    init project java

---
+++ src/hocjava/HelloWorld.java
@@ -0,0 +1,12 @@
+package hocjava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World java");
+		System.out.println("Hello World java");
+		System.out.println("Hello World java");
+		System.out.println("Hello World java");
+		System.out.println("Hello World java");
+
+	}
+}
